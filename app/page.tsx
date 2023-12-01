c4639c2e9a5503a4a6b7cb58f29a8baab147746d
Martin Sigut <martin@algorim.com>
2023-12-01 09:43:53 +0100

    missing files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Button } from '@mantine/core';
+import {Button, Image} from '@mantine/core';
 
 export default async function HomePage() {
   return (
@@ -7,10 +7,19 @@ export default async function HomePage() {
           <Button component={Link} href="/hello">
             Next link button
           </Button>
+
           <ul>
               <li><Link href="/ui">UI</Link></li>
               <li><Link href="/ui/my">UI: My</Link></li>
           </ul>
+
+          <Image
+              src="/banner/1.denver.jpg"
+              width={400}
+              height={200}
+              className="hidden md:block"
+              alt="Screenshots of the dashboard project showing desktop version"
+          />
       </>
   );
 }
--- app/ui/card/page.tsx
+++ app/ui/card/page.tsx
@@ -1,15 +1,15 @@
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { Card, CardSection, Image, Text, Badge, Button, Group } from '@mantine/core';
 
 export default function Demo() {
     return (
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-            <Card.Section>
+            <CardSection>
                 <Image
                     src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
                     height={160}
                     alt="Norway"
                 />
-            </Card.Section>
+            </CardSection>
 
             <Group justify="space-between" mt="md" mb="xs">
                 <Text fw={500}>Norway Fjord Adventures</Text>
